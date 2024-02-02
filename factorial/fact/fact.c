31dfd35e50fae742ff5737748f09a6aeed8d379c
Hermes <Hemres@upb.edu.co>
2024-02-01 23:00:58 -0500

    Corrigiendo el doble espacio que tenia la linea 32

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
     
     /*Print the result*/
 
-    printf("The factorial of %d is:  %d\n",number,factorial); ////
+    printf("The factorial of %d is: %d\n",number,factorial); ////
     
     /*Implement the function that calculates if the argument is a prime number*/
 
