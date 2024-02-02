ad81671f6b595913fc1982f1f74b4ccc6ccf730a
Hermes <Hemres@upb.edu.co>
2024-02-01 22:33:06 -0500

    Se arreglo las lines: 31,49,53. Para poder sacar mas puntaje

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
     
     /*Print the result*/
 
-    printf("El factorial de %d es %d",number,factorial);
+    printf("The factorial of %d is: %d",number,factorial); ////
     
     /*Implement the function that calculates if the argument is a prime number*/
 
@@ -46,11 +46,11 @@ int main(int argc, char *argv[]) {
 
     if(count == 1)
     {
-        printf("%d es número primo\n",number);
+        printf("%d is a prime number\n",number);///
     }
     else
     {
-        printf("%d no es número primo\n",number);
+        printf("%d is not a prime number\n",number);///
     }
     
     return 0;
