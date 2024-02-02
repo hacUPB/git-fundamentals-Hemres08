2d87dbf6c46643d697ded54606cbd3dc519b1a6f
Hermes <Hemres@upb.edu.co>
2024-02-01 22:25:17 -0500

    Corrigiendo la linea 16, para permitir sacar mas puntaje

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 
     if(number < 0) 
     {
-        perror("El núnmero debe ser mayor a cero");
+        perror("The number must be greater than or equal to zero");
         exit(1);
     } 
 
